bd41f516a1ffbe6b1015223ae2d32b8e4bd0d21f
Hyerin <hyerin.bcit@gmail.com>
2023-07-26 16:55:17 -0700

    set approuter

---
+++ react-front-end/src/routers/AppRouter.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+export default function AppRouter() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
+}
