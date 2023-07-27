583e2e55603928cb0856d5342364e105d3fc2c90
Hyerin <hyerin.bcit@gmail.com>
2023-07-27 15:04:27 -0700

    Button component update

--- react-front-end/src/components/Button.js
+++ react-front-end/src/components/Button.js
@@ -4,15 +4,23 @@ import "../styles/components/Button.scss";
 export default function Button(props) {
   const {
     title = "default",
+    width = "224px",
+    height = "58px",
     size = "23px",
     border,
     bgColor,
     color = "#1d1d1d",
+    padding = "0",
+    margin = "0",
   } = props;
   return (
     <div className="button-cont">
       <button
         style={{
+          width: width,
+          height: height,
+          padding: padding,
+          margin: margin,
           fontSize: size,
           border: border,
           background: bgColor,
