18a57a3ddce3b24b4292e83309843a948feead34
Hyerin <hyerin.bcit@gmail.com>
2023-07-30 00:50:27 -0700

    Updated Rating Profile component

--- react-front-end/src/components/RatingProfile.js
+++ react-front-end/src/components/RatingProfile.js
@@ -1,6 +1,9 @@
 import React from "react";
+
+// styles
 import "../styles/components/RatingProfile.scss";
 
+// icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,8 +12,8 @@ export default function RatingProfile({ ratingImage }) {
     <div className="rating-cont">
       <FontAwesomeIcon icon={faInfoCircle} size="lg" />
       <span>
-        Rating depends on your accumulated points!: <br></br> 🪹 → 🌱 → 🌿 → 🪴 →
-        🌳<br></br> You can get more info on the How to collect page!
+        Rating on your THIS MONTH accumulated points!: <br></br> 🪹 → 🌱 → 🌿 →
+        🪴 → 🌳<br></br> You can get more info on the How to collect page!
       </span>
       <img src={ratingImage} alt="second-rating" />
     </div>
