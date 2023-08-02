0595b4c3bbad231fb8a3988d1037bf3d8b676b44
Hyerin <hyerin.bcit@gmail.com>
2023-08-01 18:27:58 -0700

    How to collect page done

--- react-front-end/src/routers/AppRouter.js
+++ react-front-end/src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import CityDetail from "../pages/CityDetail";
+import HowTo from "../pages/HowTo";
 
 export default function AppRouter() {
   return (
@@ -9,6 +10,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="city/:id" element={<CityDetail />}></Route>
+        <Route path="/howTo" element={<HowTo />}></Route>
       </Routes>
     </Router>
   );
