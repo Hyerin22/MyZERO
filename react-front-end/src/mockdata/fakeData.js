06f3ece98df484f4b832e243de9ad21504ee6f8b
Hyerin <hyerin.bcit@gmail.com>
2023-08-02 23:34:40 -0700

    Organized fake data

---
+++ react-front-end/src/mockdata/fakeData.js
@@ -0,0 +1,166 @@
+export const fakeTop10 = [
+  {
+    id: 1,
+    name: "John Doe",
+    username: "johndoe",
+    points: 1000,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    username: "janesmith",
+    points: 1500,
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    username: "bobjohnson",
+    points: 800,
+  },
+  {
+    id: 4,
+    name: "Alice Brown",
+    username: "alicebrown",
+    points: 1200,
+  },
+  {
+    id: 5,
+    name: "Michael Lee",
+    username: "michaellee",
+    points: 1800,
+  },
+  {
+    id: 6,
+    name: "Emily Chen",
+    username: "emilychen",
+    points: 950,
+  },
+  {
+    id: 7,
+    name: "David Kim",
+    username: "davidkim",
+    points: 1400,
+  },
+  {
+    id: 8,
+    name: "Sophia Nguyen",
+    username: "sophianguyen",
+    points: 700,
+  },
+  {
+    id: 9,
+    name: "James Wilson",
+    username: "jameswilson",
+    points: 1600,
+  },
+  {
+    id: 10,
+    name: "Olivia Davis",
+    username: "oliviadavis",
+    points: 1100,
+  },
+];
+
+export const fakeProduct = [
+  {
+    id: 1,
+    productName: "Reusable Foldable Grocery Tote Bags",
+    points: 120,
+    brandName: "Walmart",
+    image: "product01.jpg",
+  },
+  {
+    id: 2,
+    productName: "Reusable K Cups For Keurig Coffee Pods",
+    points: 340,
+    brandName: "Walmart",
+    image: "product02.jpg",
+  },
+  {
+    id: 3,
+    productName: "Reusable Silicone Sponge",
+    points: 360,
+    brandName: "Walmart",
+    image: "product03.jpg",
+  },
+  {
+    id: 4,
+    productName: "Reusable Paper Towels Washable Roll",
+    points: 700,
+    brandName: "Walmart",
+    image: "product04.jpg",
+  },
+  {
+    id: 5,
+    productName: "Reusable Beeswax Food Storage Wrap",
+    points: 280,
+    brandName: "Walmart",
+    image: "product05.jpg",
+  },
+  {
+    id: 6,
+    productName: "Reusable Stainless Steel Straws",
+    points: 70,
+    brandName: "Walmart",
+    image: "product06.jpg",
+  },
+  {
+    id: 7,
+    productName: "Greenzla Reusable Gallon Bags",
+    points: 370,
+    brandName: "Superstore",
+    image: "product07.jpg",
+  },
+  {
+    id: 8,
+    productName: "Reusable Air Fryer Silicone Pot",
+    points: 410,
+    brandName: "Superstore",
+    image: "product08.jpg",
+  },
+  {
+    id: 9,
+    productName: "Reusable Silicone Baking Mat",
+    points: 360,
+    brandName: "Superstore",
+    image: "product09.jpg",
+  },
+];
+
+export const fakeHistory = [
+  {
+    id: 1,
+    date: "08/01",
+    brandName: "Walmart",
+    deductedPoints: 120,
+    remainingPoints: 2800,
+  },
+  {
+    id: 2,
+    date: "08/02",
+    brandName: "Superstore",
+    deductedPoints: 200,
+    remainingPoints: 2600,
+  },
+  {
+    id: 3,
+    date: "08/03",
+    brandName: "Superstore",
+    deductedPoints: 450,
+    remainingPoints: 2150,
+  },
+  {
+    id: 4,
+    date: "08/04",
+    brandName: "Walmart",
+    deductedPoints: 180,
+    remainingPoints: 1970,
+  },
+  {
+    id: 5,
+    date: "08/05",
+    brandName: "Walmart",
+    deductedPoints: 567,
+    remainingPoints: 1403,
+  },
+];
