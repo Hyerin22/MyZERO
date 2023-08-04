852d23a59272047e125f525941d367b63ea7ffbb
Hyerin <hyerin.bcit@gmail.com>
2023-08-04 00:23:25 -0700

    Hamburger menu update

--- react-front-end/src/components/HamburgerNav.js
+++ react-front-end/src/components/HamburgerNav.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
+// styles
 import "../styles/components/HamburgerNav.scss";
 
-export default function HamburgerNav({ isOpen, onClick }) {
+// images
+import ratingSecond from "../img/rating-second.png";
+import vancSymbol from "../img/vancouver-symbol.jpeg";
+
+export default function HamburgerNav({
+  isOpen,
+  onClick,
+  username = "User",
+  ratingImg = ratingSecond,
+  cityImg = vancSymbol,
+}) {
   const location = useLocation();
   const currentPathname = location.pathname;
 
   return (
     <div className="hamNav-cont">
-      <button className="hamNav" onClick={onClick}>
-        <div className={isOpen ? "line1After" : "line1"}></div>
-        <div className={isOpen ? "line2After" : "line2"}></div>
-        <div className={isOpen ? "line3After" : "line3"}></div>
-      </button>
+      <div className="ham-first">
+        <div className="ham-infoIcon rating">
+          <img src={ratingImg} alt="rating" />
+        </div>
+        <div className="ham-infoIcon city">
+          <img src={cityImg} alt="city" />
+        </div>
+      </div>
+      <div className="ham-second">
+        <p>Hi, {username}</p>
+        <button className="hamNav" onClick={onClick}>
+          <div className={isOpen ? "line1After" : "line1"}></div>
+          <div className={isOpen ? "line2After" : "line2"}></div>
+          <div className={isOpen ? "line3After" : "line3"}></div>
+        </button>
+      </div>
 
       {isOpen && (
         <div className="link-cont">
