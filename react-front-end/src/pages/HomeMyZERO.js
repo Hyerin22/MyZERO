246cbbbbe0b1432cc713daca51415b9d1cb524e4
ktehi21 <k.tehi21@gmail.com>
2023-08-03 19:41:57 -0700

    connect the monthly point and prev 3 months point data with chart

--- react-front-end/src/pages/HomeMyZERO.js
+++ react-front-end/src/pages/HomeMyZERO.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 // styles
 import "../styles/HomeMyZERO.scss";
@@ -11,7 +21,118 @@ import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/Button";
 import DisplayPointTxt from "../components/DisplayPointTxt";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true
+};
+
 export default function HomeMyZERO() {
+
+  const [state, setState] = useState({
+    id: 1,
+    three_month: {},
+    two_month: {},
+    one_month: {},
+    this_month: {}
+  });
+
+  const [selectedMonths, setSelectedMonths] = useState([]);
+  const currentMonth = new Date().getMonth() + 1;
+
+  // Calculate the recent 4 months from the current month
+  useEffect(() => {
+    const recentMonths = [];
+    for (let i = 0; i < 4; i++) {
+      // Handle cases when current month is less than 4
+      recentMonths.push((currentMonth - i + 12) % 12);
+    }
+    // console.log("recentMonths:", recentMonths);
+
+    setSelectedMonths(recentMonths);
+  }, [currentMonth]);
+
+  const getMonthName = (monthNumber) => {
+    if (monthNumber == '1') return 'January';
+    else if (monthNumber == '2') return 'February';
+    else if (monthNumber == '3') return 'March';
+    else if (monthNumber == '4') return 'April';
+    else if (monthNumber == '5') return 'May';
+    else if (monthNumber == '6') return 'June';
+    else if (monthNumber == '7') return 'July';
+    else if (monthNumber == '8') return 'August';
+    else if (monthNumber == '9') return 'September';
+    else if (monthNumber == '10') return 'October';
+    else if (monthNumber == '11') return 'November';
+    else if (monthNumber == '12') return 'December';
+    else return 'Invalid Month';
+  }
+  // Points collected this month
+  useEffect(() => {
+    axios.get(`/api/points/${state.id}/month?months=${selectedMonths.join(',')}`)
+      .then((res) => {
+
+
+
+        // Convert numeric month to English name
+        const formattedData = res.data.map((item) => ({
+          month: getMonthName(item.month),
+          month_points: item.month_points,
+        }));
+        console.log("getMonthName", formattedData);
+        const three_month = formattedData[0];
+        const two_month = formattedData[1];
+        const one_month = formattedData[2];
+        const this_month = formattedData[3];
+
+        setState(prev => ({
+          ...prev,
+          three_month,
+          two_month,
+          one_month,
+          this_month
+        }));
+      })
+      .catch(err => {
+        console.error("connect error:", err.message);
+      });
+  }, []);
+
+  const months = [
+    state.three_month.month,
+    state.two_month.month,
+    state.one_month.month,
+    state.this_month.month
+  ];
+  
+  const points = [
+    state.three_month.month_points,
+    state.two_month.month_points,
+    state.one_month.month_points,
+    state.this_month.month_points
+  ];
+  
+  const data = {
+    labels: months, 
+    datasets: [
+      {
+        label: 'Points',
+        data: points,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, .2)',
+        borderWidth: 1,
+      },
+    ],
+  };
+  console.log('data', data)
+
+
   return (
     <div className="myZero-cont">
       <div className="myZero-top">
@@ -25,7 +146,7 @@ export default function HomeMyZERO() {
         />
         <DisplayPointTxt
           text="This month you collected"
-          point="00"
+          point={state.this_month.month_points}
           size="24px"
           pointSize="64px"
           alignItems="center"
@@ -34,13 +155,15 @@ export default function HomeMyZERO() {
       </div>
       <div className="myZero-mid">
         <p className="heading">History</p>
-        <div className="graph-cont">Graph here</div>
+        <div className="graph-cont">
+          <Bar options={options} data={data} />
+        </div>
       </div>
       <div className="myZero-mid">
         <div className="graph-cont2">
           <FontAwesomeIcon icon={faInfoCircle} size="lg" />
           <span>Trees collected since you joined</span>
-          Trees are here
+          Still, we have a lot of seeds🪹. Let's try to grow the trees!
         </div>
       </div>
     </div>
